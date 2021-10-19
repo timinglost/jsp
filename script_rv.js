8e4cac833d295945a17d33aa7e53b79654d3b57c
Kirill <budanovkirilldmitrievich@gmail.com>
2021-10-19 14:55:08 +0300

    dz

--- script_rv.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-let str = document.querySelector('.text_main');
-let use_str = str.textContent;
-let reg = /\B'|'\B/g;
-let new_str = use_str.replace(reg, '"');
-str.textContent = new_str
